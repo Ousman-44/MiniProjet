d2d0bfb78b5b86c6a7b6fdb5c297d28f590b0f14
Ousman-44 <gueraousman@gmail.com>
2026-03-04 14:28:33 +0100

    creation Fournisseur

---
+++ src/main/java/pharmacie/entity/Fournisseur.java
@@ -0,0 +1,52 @@
+package comptoirs.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(
+    name = "fournisseurs",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_fournisseur_email", columnNames = {"email"})
+    }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Fournisseur {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Column(nullable = false)
+  private String nom;
+
+  @NotBlank
+  @Email
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  /**
+   * Un fournisseur peut fournir plusieurs catégories
+   * et une catégorie peut être fournie par plusieurs fournisseurs.
+   */
+  @ManyToMany
+  @JoinTable(
+      name = "fournisseur_categories",
+      joinColumns = @JoinColumn(name = "fournisseur_id"),
+      inverseJoinColumns = @JoinColumn(name = "categorie_id")
+  )
+  @Builder.Default
+  @JsonIgnore // évite boucles JSON si tu exposes directement les entités
+  private Set<Categorie> categories = new HashSet<>();
+}
